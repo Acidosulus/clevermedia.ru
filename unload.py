205d588718108282248c442e72423113747e3a50
Acidosulus <acidos@yandex.ru>
2022-09-05 00:56:11 +0700

    0.2

--- unload.py
+++ unload.py
@@ -53,6 +53,9 @@ if sys.argv[1] == 'catalog':
     for link in links_list:
         lo_good = unload_one_good(wd, link, sys.argv[3])
         lc_name = lo_good.name if lo_good.name.count(lo_good.article) != 0 else lo_good.article + ' ' + lo_good.name
+        if 'Под заказ' in lo_good.description:
+            echo(style('Под заказ', fg='bright_red'))
+            continue
         price.add_good('',
                                 prepare_str(lc_name),
                                 prepare_str(lo_good.description),
